4509cbe5446d1465f2848081e444ccea570ea76f
Arke <arke@iwalnuts.com>
2016-10-25 12:38:42 +0800

    上传程序文件

---
+++ app/Http/Controllers/Customer/CustomerController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Controllers\Customer;
+use App\Http\Controllers\Controller;
+
+class CustomerController extends Controller
+{
+	public function index()
+    {
+		echo 'hehe';
+    }
+}
